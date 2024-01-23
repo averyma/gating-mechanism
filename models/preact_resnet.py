cafe2976bf57a635703ec63db6bfb9eab7aa2e47
ama <avery.mabojie@gmail.com>
2024-01-22 21:49:55 -0500

    added shallow network (removing layer3) and gate v2

--- models/preact_resnet.py
+++ models/preact_resnet.py
@@ -97,9 +97,41 @@ class PreActResNet(nn.Module):
         out = self.linear(out)
         return out
 
-class PreActResNet_gate(nn.Module):
+class PreActResNet_s(nn.Module):
     def __init__(self, block, num_blocks, num_classes=10):
-        super(PreActResNet_gate, self).__init__()
+        super(PreActResNet_s, self).__init__()
+        self.in_planes = 64
+
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
+        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
+        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
+        # self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
+        # self.avgpool = nn.AvgPool2d(4)
+        self.avgpool = nn.AvgPool2d(8)
+        self.linear = nn.Linear(256*block.expansion, num_classes)
+
+    def _make_layer(self, block, planes, num_blocks, stride):
+        strides = [stride] + [1]*(num_blocks-1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.in_planes, planes, stride))
+            self.in_planes = planes * block.expansion
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        out = self.conv1(x)
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.avgpool(out)
+        out = out.view(out.size(0), -1)
+        out = self.linear(out)
+        return out
+
+class PreActResNet_gate_v1(nn.Module):
+    def __init__(self, block, num_blocks, num_classes=10):
+        super(PreActResNet_gate_v1, self).__init__()
         self.in_planes = 64
 
         self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
@@ -171,26 +203,109 @@ class PreActResNet_gate(nn.Module):
                                  logit4.unsqueeze(2)], dim=2)
         return concatgate, concatlogit
 
-def PreActResNet18(gate, num_classes=10):
-    if not gate:
-        return PreActResNet(PreActBlock, [2,2,2,2], num_classes)
-    else:
-        return PreActResNet_gate(PreActBlock, [2,2,2,2], num_classes)
+class PreActResNet_gate_v2(nn.Module):
+    def __init__(self, block, num_blocks, num_classes=10):
+        super(PreActResNet_gate_v2, self).__init__()
+        self.in_planes = 64
+
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
+        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
+        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
+        self.avgpool = nn.AvgPool2d(4)
+        # self.linear = nn.Linear(512*block.expansion, num_classes)
+
+        # added for gated mechanism
+        sigmoid = nn.Sigmoid()
+        d1 = 64 * block.expansion * 32**2
+        d2 = 128 * block.expansion * 16**2
+        d3 = 256 * block.expansion * 8**2
+        d4 = 512 * block.expansion
+
+        self.Gate1 = nn.Sequential(nn.Linear(d1, 1), sigmoid, nn.Unflatten(1, (1, 1, 1)))
+        self.Gate2 = nn.Sequential(nn.Linear(d2, 1), sigmoid, nn.Unflatten(1, (1, 1, 1)))
+        self.Gate3 = nn.Sequential(nn.Linear(d3, 1), sigmoid, nn.Unflatten(1, (1, 1, 1)))
+        self.Gate4 = nn.Sequential(nn.Linear(d4, 1), sigmoid, nn.Unflatten(1, (1, 1, 1)))
+
+        self.fc1 = nn.Linear(d1, num_classes)
+        self.fc2 = nn.Linear(d2, num_classes)
+        self.fc3 = nn.Linear(d3, num_classes)
+        self.fc4 = nn.Linear(d4, num_classes)
+
+    def _make_layer(self, block, planes, num_blocks, stride):
+        strides = [stride] + [1]*(num_blocks-1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.in_planes, planes, stride))
+            self.in_planes = planes * block.expansion
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.conv1(x)
+
+        # gate = torch.ones([x.shape[0], 1, 1, 1], device=x.device)
+        x = self.layer1(x)
+        x_flatten = torch.flatten(x, 1)
+        gate1 = self.Gate1(x_flatten)
+        # gate = gate*(1 - gate1)
+        logit1 = self.fc1(x_flatten * (1-gate1).flatten(1))
+
+        x = self.layer2(x * gate1)
+        x_flatten = torch.flatten(x, 1)
+        gate2 = self.Gate2(x_flatten)
+        # gate = gate*(1 - gate2)
+        logit2 = self.fc2(x_flatten * (1-gate2).flatten(1))
+
+        x = self.layer3(x * gate2)
+        x_flatten = torch.flatten(x, 1)
+        gate3 = self.Gate3(x_flatten)
+        # gate = gate*(1 - gate3)
+        logit3 = self.fc3(x_flatten * (1-gate3).flatten(1))
+
+        x = self.layer4(x * gate3)
+
+        x = self.avgpool(x)
+        x_flatten = torch.flatten(x, 1)
+        gate4 = self.Gate4(x_flatten)
+        logit4 = self.fc4(x_flatten)
+
+        concatgate = torch.cat([gate1, gate2, gate3, gate4], dim=1).squeeze()
+        concatlogit = torch.cat([logit1.unsqueeze(2),
+                                 logit2.unsqueeze(2),
+                                 logit3.unsqueeze(2),
+                                 logit4.unsqueeze(2)], dim=2)
+        return concatgate, concatlogit
+
+def PreActResNet18(gate, shallow, num_classes=10):
+    if shallow:
+        return PreActResNet_s(PreActBlock, [2,2,2,2], num_classes)
+    elif gate == 1:
+        return PreActResNet_gate_v1(PreActBlock, [2,2,2,2], num_classes)
+    elif gate == 2:
+        return PreActResNet_gate_v2(PreActBlock, [2,2,2,2], num_classes)
+    return PreActResNet(PreActBlock, [2,2,2,2], num_classes)
+
+def PreActResNet50(gate, shallow, num_classes=10):
+    if shallow:
+        return PreActResNet_s(PreActBottleneck, [3,4,6,3], num_classes)
+    elif gate == 1:
+        return PreActResNet_gate_v1(PreActBottleneck, [3,4,6,3], num_classes)
+    elif gate == 2:
+        return PreActResNet_gate_v2(PreActBottleneck, [3,4,6,3], num_classes)
+    return PreActResNet(PreActBottleneck, [3,4,6,3], num_classes)
+
+def PreActResNet101(gate, shallow, num_classes=10):
+    if shallow:
+        return PreActResNet_s(PreActBottleneck, [3,4,23,3], num_classes)
+    elif gate == 1:
+        return PreActResNet_gate_v1(PreActBottleneck, [3,4,23,3], num_classes)
+    elif gate == 2:
+        return PreActResNet_gate_v2(PreActBottleneck, [3,4,23,3], num_classes)
+    return PreActResNet(PreActBottleneck, [3,4,23,3], num_classes)
 
 def PreActResNet34():
     return PreActResNet(PreActBlock, [3,4,6,3])
-
-def PreActResNet50(gate, num_classes=10):
-    if not gate:
-        return PreActResNet(PreActBottleneck, [3,4,6,3], num_classes)
-    else:
-        return PreActResNet_gate(PreActBottleneck, [3,4,6,3], num_classes)
-
-def PreActResNet101(gate, num_classes=10):
-    if not gate:
-        return PreActResNet(PreActBottleneck, [3,4,23,3], num_classes)
-    else:
-        return PreActResNet_gate(PreActBottleneck, [3,4,23,3], num_classes)
 
 def PreActResNet152():
     return PreActResNet(PreActBottleneck, [3,8,36,3])
